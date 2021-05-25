c3d425af6e3dba87f41e9eefe78d7b58bad4783f
vsmaliakou <vsmaliakou@yandex.by>
2021-05-25 11:19:02 +0300

    Updates

---
+++ precache-manifest.d6beb2c43726bd46757dd1695e01176e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5981d4fcced0647a0fe03115b1c1dd06",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "b0e6cc8be52cb60d3026",
+    "url": "/counter/static/css/main.66c702be.chunk.css"
+  },
+  {
+    "revision": "474f47eeb4006f76e99c",
+    "url": "/counter/static/js/2.830dc66c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/counter/static/js/2.830dc66c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e6cc8be52cb60d3026",
+    "url": "/counter/static/js/main.6f4c7d00.chunk.js"
+  },
+  {
+    "revision": "41ab835c22a2ec780517",
+    "url": "/counter/static/js/runtime-main.c9bcdafb.js"
+  }
+]);
